74a70810b8bf8965bfa59440f47184f73e691538
Sree <srimaye.nallamsetty@gmail.com>
2024-11-15 13:58:44 +0000

    Berries scenario added

--- cypress/e2e/PokeAPI/pikachu.cy.ts
+++ cypress/e2e/PokeAPI/pikachu.cy.ts
@@ -1,10 +1,23 @@
 describe('PokeAPI Tests', () => {
     it('should fetch data of a specific Pokémon', () => {
-      cy.request('https://pokeapi.co/api/v2/pokemon/pikachu').then((response) => {
+      // give endpoint - pokemon/pikachu
+      cy.request("GET", "/pokemon/pikachu").then((response) => {
         expect(response.status).to.eq(200);
         expect(response.body).to.have.property('name', 'pikachu');
         expect(response.body).to.have.property('id', 25);
       });
     });
-  });
-  
+
+    it('should fetch data of a specific berry', () => {
+      // give endpoint - berry
+      cy.request("GET", "/berry/1").then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('name', 'cheri');
+        expect(response.body).to.have.property('id', 1);
+        expect(response.body.flavors[0]).to.have.property('potency', 10);
+        expect(response.body.flavors[0].flavor).to.have.property('name', 'spicy');
+        expect(response.body.firmness).to.have.property('name', 'soft');
+      });
+      
+    });
+  }); 
